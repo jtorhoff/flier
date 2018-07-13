34bb877ad3c345d9a62e43a3eea900bb8506b9d4
Abhishek Koserwal <akoserwa@redhat.com>
2018-07-14 02:14:50 +0530

    fix build: module path & typedef of rusha module

--- typings/rusha.d.ts
+++ typings/rusha.d.ts
@@ -4,7 +4,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-declare module "Rusha" {
+declare module "rusha" {
     interface Rusha {
         new(): this;
         rawDigest(buffer: Uint8Array): Uint32Array;
